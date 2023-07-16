b0f594794a4e3c00e1073437a0f5769c31663dcd
jx1062142769 <jx1062142769@gmail.com>
2023-07-17 01:50:24 +0800

    feature/add simp chinese localisation and focus content

--- tutorial/dates.py
+++ tutorial/dates.py
@@ -5,23 +5,21 @@ test_path = "D:\\hoi4mod\\test"
 ins_path = "C:\\Users\\JacobXij\\Documents\\Paradox Interactive\\Hearts of Iron IV\\mod\\EastMeme\\tutorial\\"
 path = 'C:\\Users\\JacobXij\\Documents\\Paradox Interactive\\Hearts of Iron IV\\mod\\EastMeme\\'
 
-sub_path = 'common\\technologies\\'
+sub_path = 'tutorial\\replace\\'
 
 origin_date_list = []
-for i in range(1918, 1956):
-	origin_date_list.append(str(i))
-delta = 84
-
-print(origin_date_list)
-for file_name in os.listdir(path + sub_path):
-	technology_content = ''
-	with open(path + sub_path + file_name, 'r', encoding='utf-8-sig') as technologyFile:
-		technology_data = technologyFile.readlines();
-		for line in technology_data:
-			new_line = line
-			for origin_date in origin_date_list:
-				target_date = str(int(origin_date) + delta)
-				new_line = new_line.replace(origin_date, target_date)
-			technology_content += new_line
-	with open(path + sub_path + file_name, 'w', encoding='utf-8') as technologyFile:
-		technologyFile.write(technology_content)
+fileNameList = os.listdir(path + sub_path)
+for file_name in fileNameList:
+	new_localisation_data = ''
+	with open(path + sub_path + file_name, 'r', encoding='utf-8-sig') as localisationFile:
+		localisation_data = localisationFile.readlines()
+		index = 0
+		for data in localisation_data:
+			if index == 0:
+				data = data.replace("english", "simp_chinese")
+			new_localisation_data += data
+			index += 1
+	with open(path + sub_path + file_name, 'w', encoding='utf-8-sig') as localisationFile:
+		localisationFile.write(new_localisation_data)
+	new_name = file_name.replace("english", "simp_chinese")
+	os.rename(path + sub_path + file_name, path + sub_path + new_name)
